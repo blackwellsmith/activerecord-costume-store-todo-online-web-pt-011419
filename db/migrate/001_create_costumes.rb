91822d07ddb22ce131588102b879a595ae34af6e
blackwell <blackwellsmithiv@gmail.com>
2019-04-02 13:19:58 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,11 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :breed
+      t.integer :price
+      t.integer :size 
+      t.string :url
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
