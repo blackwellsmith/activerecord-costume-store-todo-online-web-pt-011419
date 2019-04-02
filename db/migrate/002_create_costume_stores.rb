863afd72c9dead3cdbd34793dfd6a41976fd40fe
blackwell <blackwellsmithiv@gmail.com>
2019-04-02 13:50:36 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size 
+      t.string :url
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
