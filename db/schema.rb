0d423ec04264f544dcaa2e1f1f8546fe3a7cbf51
blackwell <blackwellsmithiv@gmail.com>
2019-04-02 14:05:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.integer "size"
-    t.string "url"
+    t.string "size"
+    t.string "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
